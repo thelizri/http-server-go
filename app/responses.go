fda53402a2dc9246ef83957a81d8d7a65c5e2ab5
Karlan <karl.william.tony.carlstedt@gmail.com>
2024-07-01 21:07:40 +0200

    Refactored a little bit. Added more http responses

--- app/responses.go
+++ app/responses.go
@@ -1,3 +1,15 @@
 package main
 
-const RESPONSE_200 string = "HTTP/1.1 200 OK\r\n\r\n"
+const RESPONSE_OK string = "HTTP/1.1 200 OK\r\n\r\n"
+const RESPONSE_CREATED string = "HTTP/1.1 201 Created\r\n\r\n"
+const RESPONSE_NO_CONTENT string = "HTTP/1.1 204 No Content\r\n\r\n"
+const RESPONSE_MOVED_PERMANENTLY string = "HTTP/1.1 301 Moved Permanently\r\n\r\n"
+const RESPONSE_FOUND string = "HTTP/1.1 302 Found\r\n\r\n"
+const RESPONSE_BAD_REQUEST string = "HTTP/1.1 400 Bad Request\r\n\r\n"
+const RESPONSE_UNAUTHORIZED string = "HTTP/1.1 401 Unauthorized\r\n\r\n"
+const RESPONSE_FORBIDDEN string = "HTTP/1.1 403 Forbidden\r\n\r\n"
+const RESPONSE_NOT_FOUND string = "HTTP/1.1 404 Not Found\r\n\r\n"
+const RESPONSE_METHOD_NOT_ALLOWED string = "HTTP/1.1 405 Method Not Allowed\r\n\r\n"
+const RESPONSE_INTERNAL_SERVER_ERROR string = "HTTP/1.1 500 Internal Server Error\r\n\r\n"
+const RESPONSE_BAD_GATEWAY string = "HTTP/1.1 502 Bad Gateway\r\n\r\n"
+const RESPONSE_SERVICE_UNAVAILABLE string = "HTTP/1.1 503 Service Unavailable\r\n\r\n"
--- app/server.go
+++ app/server.go
@@ -41,30 +41,31 @@ func handleConnection(conn net.Conn) {
 	buffer := make([]byte, 1024)
 
 	for {
-		conn.SetReadDeadline(time.Now().Add(2 * time.Second))
+		//Set timeout
+		conn.SetReadDeadline(time.Now().Add(300 * time.Millisecond))
 		n, err := conn.Read(buffer)
 
+		//Handle time out
 		if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
 			fmt.Println("read timeout:", err)
 			break
 		}
 
+		//Handle error
 		if err != nil {
-			panic("Cannot read data into buffer")
+			fmt.Errorf("Cannot read data into buffer, %v", err)
 		}
 
-		if n == 0 {
-			break
-		}
-
-		request, headers, body := extractParts(string(buffer))
+		request, headers, body := extractParts(string(buffer[:n]))
 		fmt.Printf("Request is here: %s\n\n", request)
 		fmt.Printf("Headers is here: %s\n\n", headers)
 		fmt.Printf("Body is here: %s\n\n", body)
+		url := extractURL(request)
+		fmt.Printf("URL is here: %s\n\n", url)
 	}
 
 	// Send data to the client
-	data := []byte(RESPONSE_200)
+	data := []byte(RESPONSE_OK)
 	_, err := conn.Write(data)
 	if err != nil {
 		fmt.Println("Error:", err)
@@ -72,20 +73,23 @@ func handleConnection(conn net.Conn) {
 	}
 }
 
-func extractParts(value string) (string, string, string) {
+func extractParts(value string) (request, headers, body string) {
 	fmt.Println()
 	// Split the input value into headers and body
 	parts := strings.Split(value, "\r\n\r\n")
 	headersPart := parts[0]
-	body := ""
 	if len(parts) > 1 {
 		body = parts[1]
 	}
 
 	// Split headers part into request line and header lines
 	lines := strings.Split(headersPart, "\r\n")
-	request := strings.Split(lines[0], " ")[1]
-	headers := strings.Join(lines[1:], "\r\n")
+	request = lines[0]
+	headers = strings.Join(lines[1:], "\r\n")
 
 	return request, headers, body
+}
+
+func extractURL(request string) string {
+	return strings.Split(request, " ")[1]
 }
